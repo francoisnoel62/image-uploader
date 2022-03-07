ff2ac0de7a1dcb2133f6daa516dc3d4e16d12ebb
francoisnoel62 <francoisnoel62@gmail.com>
2022-03-07 15:40:15 +0100

    filter file type on input file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCOmcx2sqr6vadPqbSMF5GsxykGCy8fEeY",
+    authDomain: "image--uploader-6d7e1.firebaseapp.com",
+    projectId: "image--uploader-6d7e1",
+    storageBucket: "image--uploader-6d7e1.appspot.com",
+    messagingSenderId: "980927604096",
+    appId: "1:980927604096:web:84d2025304b88455118d65"
+  }
 };
